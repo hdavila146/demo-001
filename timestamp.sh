6b5ec4eb1cd04a633b4065a98066f75bd536e017
root <root@USFLA0563.XSM.local>
2021-07-18 20:35:05 -0300

    my first commit

---
+++ timestamp.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Define a timestamp function
+timestamp() {
+  date  +"%F-%T" # current time
+}
+timestamp
---
+++ vars.tf
@@ -0,0 +1,19 @@
+variable "aws_region" {
+    type = string 
+    default = "us-east-1"
+}
+variable "bucket_name" {
+  description = "The name of the bucket"
+  type = string
+  default   = "test-bucket-hdavila146"
+}
+variable "file_name1" {
+  description = "The name of the bucket"
+  type = string
+  default   = "test1"
+}
+variable "file_name2" {
+  description = "The name of the bucket"
+  type = string
+  default   = "tes2"
+}
